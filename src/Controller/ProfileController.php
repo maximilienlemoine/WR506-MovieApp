9f2434cce954cef2eabf0b7fd5bade07bf447da7
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-02-18 12:30:42 +0100

    feat : forgor password

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,9 +6,12 @@ use App\Entity\User;
 use App\Repository\MediaObjectRepository;
 use App\Repository\UserRepository;
 use App\Serializer\MediaObjectNormalizer;
+use App\Service\MailerService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -19,17 +22,20 @@ class ProfileController extends AbstractController
     private UserRepository $userRepository;
     private MediaObjectNormalizer $mediaObjectNormalizer;
     private MediaObjectRepository $mediaObjectRepository;
+    private MailerService $mailerService;
     private UserPasswordHasherInterface $passwordHasher;
 
     public function __construct(
         UserRepository $userRepository,
         MediaObjectNormalizer $mediaObjectNormalizer,
         MediaObjectRepository $mediaObjectRepository,
+        MailerService $mailerService,
         UserPasswordHasherInterface $passwordHasher,
     ) {
         $this->userRepository = $userRepository;
         $this->mediaObjectNormalizer = $mediaObjectNormalizer;
         $this->mediaObjectRepository = $mediaObjectRepository;
+        $this->mailerService = $mailerService;
         $this->passwordHasher = $passwordHasher;
     }
 
@@ -119,5 +125,67 @@ class ProfileController extends AbstractController
         ];
 
         return new JsonResponse($userData);
+    }
+
+    //Forgot PASSWORD
+
+    /**
+     * @throws Exception
+     * @throws TransportExceptionInterface
+     */
+    #[Route(
+        "/api/password/forgot",
+        name: "forgot_password",
+        methods: ["POST"]
+    )]
+    public function forgotPassword(Request $request): JsonResponse
+    {
+        $email = json_decode($request->getContent(), true)['email'];
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+        if (!$user) {
+            return new JsonResponse(['message' => 'User not found'], 404);
+        }
+        $user->setResetToken(bin2hex(random_bytes(32)));
+        $this->userRepository->saveUser($user);
+
+        $this->mailerService->sendEmail(
+            $user->getEmail(),
+            'Mot de passe oublié',
+            sprintf('Movie App :
+            Pour réinitialiser votre mot de passe, cliquez sur le lien suivant : 
+            %s/reset-password/%s', $this->getParameter('FRONT_URL'), $user->getResetToken())
+        );
+
+        return new JsonResponse(['message' => 'Email sent']);
+    }
+
+    //RESET PASSWORD
+
+    #[Route(
+        "/api/password/reset",
+        name: "reset_password",
+        methods: ["POST"]
+    )]
+    public function resetPassword(Request $request): JsonResponse
+    {
+        $requestContent = json_decode($request->getContent(), true);
+        $token = $requestContent['token'];
+        $user = $this->userRepository->findOneBy(['resetToken' => $token]);
+        if (!$user) {
+            return new JsonResponse(['message' => 'User not found'], 404);
+        }
+
+        $password = $requestContent['password'];
+        $confirmPassword = $requestContent['confirmPassword'];
+
+        if ($password !== $confirmPassword) {
+            return new JsonResponse(['message' => 'Passwords do not match'], 400);
+        }
+
+        $user->setResetToken(null);
+        $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+        $this->userRepository->saveUser($user);
+
+        return new JsonResponse(['message' => 'Password updated']);
     }
 }
