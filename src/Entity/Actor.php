71f7e65e0612d750d7f7c1b051022bac12373a96
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-02-18 10:52:45 +0100

    feat : crud Actor

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -2,37 +2,70 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Repository\ActorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Get(),
+        new Delete(),
+        new Patch(),
+    ],
+    normalizationContext: [
+        'groups' => ['actor:read'],
+    ],
+    denormalizationContext: [
+        'groups' => ['actor:write'],
+    ],
+    paginationClientEnabled: true,
+)]
+#[ApiFilter(SearchFilter::class, properties: ['lastName' => 'partial'])]
 class Actor
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['actor:read', 'movie:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['actor:read', 'actor:write', 'movie:read'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['actor:read', 'actor:write', 'movie:read'])]
     private ?string $lastName = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    #[Groups(['actor:read', 'actor:write', 'movie:read'])]
     private ?\DateTimeInterface $birthday = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['actor:read', 'actor:write', 'movie:read'])]
     private ?string $reward = null;
 
     #[ORM\ManyToOne(inversedBy: 'actors')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['actor:read', 'actor:write', 'movie:read'])]
     private ?Nationality $nationality = null;
 
     #[ORM\ManyToMany(targetEntity: Movie::class, mappedBy: 'actors')]
+    #[Groups(['actor:read'])]
     private Collection $movies;
 
     public function __construct()
