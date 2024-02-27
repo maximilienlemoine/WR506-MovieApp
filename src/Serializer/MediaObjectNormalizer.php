2f9e5ebc7e89e9c4e4c7e3b6ddb36ddbc2c0a8b7
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-02-27 21:19:43 +0100

    hotfix : url mediaobject

--- src/Serializer/MediaObjectNormalizer.php
+++ src/Serializer/MediaObjectNormalizer.php
@@ -3,28 +3,26 @@
 namespace App\Serializer;
 
 use App\Entity\MediaObject;
-use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Vich\UploaderBundle\Storage\StorageInterface;
 
-final class MediaObjectNormalizer implements NormalizerAwareInterface
+final class MediaObjectNormalizer implements ContextAwareNormalizerInterface, NormalizerAwareInterface
 {
+    use NormalizerAwareTrait;
+
     private const ALREADY_CALLED = 'MEDIA_OBJECT_NORMALIZER_ALREADY_CALLED';
 
-    public function __construct(private StorageInterface $storage, private NormalizerInterface $normalizer)
+    public function __construct(private StorageInterface $storage)
     {
     }
 
-    /**
-     * @throws ExceptionInterface
-     */
-    public function normalize($object, string|null $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
+    public function normalize($object, ?string $format = null, array $context = []): mixed
     {
         $context[self::ALREADY_CALLED] = true;
 
-        $object->contentUrl = $_ENV['APP_URL'] . $this->storage->resolveUri($object, 'file');
+        $object->contentUrl =  $_ENV['APP_URL'] . $this->storage->resolveUri($object, 'file');
 
         return $this->normalizer->normalize($object, $format, $context);
     }
@@ -36,17 +34,5 @@ final class MediaObjectNormalizer implements NormalizerAwareInterface
         }
 
         return $data instanceof MediaObject;
-    }
-
-    public function getSupportedTypes(?string $format): array
-    {
-        return [
-            MediaObject::class => true,
-        ];
-    }
-
-    public function setNormalizer(NormalizerInterface $normalizer): void
-    {
-        $this->normalizer = $normalizer;
     }
 }
